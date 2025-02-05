26046a9906058b34dd220b6cced4a4ffae67a917
SuperSnarfy <85713901+SuperSnarfy@users.noreply.github.com>
2025-02-04 22:35:33 -0700

    Add files via upload
    WALL JUMPS WILL WORK! or will they? Nah who am I kidding they work. Also changed the wall-sliding mechanics to prevent an infinite climbing exploit. Additionally, I modified the dash so it's now pretty fast instead of OH SHIT IMMA RUN INTO THE WALL NOW IMMEDIATELY ACCELERATE TO THE SPEED OF SOUND fast. Finally, acceleration/deceleration mechanics have been tweaked for compatibility with the wall-jump's large initial velocity.

--- do it then no balls - renee/scripts/player.gd
+++ do it then no balls - renee/scripts/player.gd
@@ -15,16 +15,18 @@ var can_dash = true
 
 func _physics_process(delta: float) -> void:
 	#MOVEMENT CONTROLS
+	#I moved the direction check up a bit b/c I need it to wall jump and to change how we do wall slides.
+	var direction := Input.get_axis("ui_left", "ui_right")
 	#gravity
-	if is_on_wall() and velocity.y >=0 and not is_on_floor():
+	if is_on_wall() and velocity.y >=0 and not is_on_floor() and direction:
 		if first_wall == false:
 			velocity.y = 0
 			first_wall = true
 		if first_wall == true:
-			velocity.y += 200*delta
+			velocity.y += 400*delta
 			can_dash = true
 	elif not is_on_floor():
-		velocity.y += 1000*delta
+		velocity.y += 1500*delta
 		first_wall = false
 	else:
 		first_wall = false
@@ -36,20 +38,23 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
 		if is_on_floor():
 			velocity.y = JUMP_VELOCITY
-		if double_jump == true and not is_on_floor() and double_jump_unlocked == true:
+		elif is_on_wall():
+			velocity.y = JUMP_VELOCITY
+			velocity.x = -1800*direction
+			#if you want to give the player the double jump back when they walljump, add double_jump == true here.
+		elif double_jump == true and double_jump_unlocked == true:
 			velocity.y = JUMP_VELOCITY
 			double_jump = false
 	#left and right
-	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction and is_dashing == false:
-		velocity.x = direction * SPEED
+		velocity.x = move_toward(velocity.x, direction * SPEED, SPEED/4)
 	else:
 		if is_dashing == false:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
 	#dash
 	if is_dashing == true:
 		while dash_timer >= 0:
-			velocity.x += 200*direction
+			velocity.x += 40*direction
 			dash_timer -=delta
 	if dash_timer <=0:
 		is_dashing = false
